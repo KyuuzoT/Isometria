543d7294e09188796d547feddc8c3dd842bfc559
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-08-03 10:45:23 +0300

    GUI started

---
+++ Assets/Scripts/AdditionalSystems/GUI/EnemyHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] private Texture2D _healthBarFrame;
+    [SerializeField] private Rect _healthBarFramePosition;
+
+    [SerializeField] private Texture2D _healthBarPoint;
+    [SerializeField] private Rect _healthBarPointPosition;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
