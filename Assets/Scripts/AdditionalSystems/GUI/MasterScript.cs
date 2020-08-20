de2791f3e8d5438d42d8ff3a85193d4a47fb6817
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-08-20 20:10:17 +0300

    Added masterScript

---
+++ Assets/Scripts/AdditionalSystems/GUI/MasterScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.GetComponent<Inventory>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.I))
+        {
+            gameObject.GetComponent<Inventory>().enabled = !gameObject.GetComponent<Inventory>().enabled;
+        }
+    }
+}
