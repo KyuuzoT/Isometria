c2b4ca5b241e53fa5e5ecc93299efcc41950e626
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-08-25 18:31:12 +0300

    Animation system and animations enum added

---
+++ Assets/Scripts/New System/Additional Systems/Animations/AnimationSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSystem : MonoBehaviour
+{
+    public AnimationClip Animation;
+
+    [SerializeField] private List<AnimationClip> _animations;
+
+    void Start()
+    {
+        if(_animations.Count == 0)
+        {
+            //TODO: exception
+            //throw new System.Exception
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Equals("attack")
+
+    }
+
+    internal AnimationClip ReturnAnimationClip(AnimationStates state)
+    {
+        switch (state)
+        {
+            case AnimationStates.Attack:
+                Animation = _animations.Find(s => s.name.Contains("attack"));
+                break;
+            case AnimationStates.Die:
+                Animation = _animations.Find(s => s.name.Contains("die"));
+                break;
+            case AnimationStates.GetHit:
+                Animation = _animations.Find(s => s.name.Equals("gethit"));
+                break;
+            case AnimationStates.Idle:
+                Animation = _animations.Find(s => s.name.Equals("idle"));
+                break;
+            case AnimationStates.Run:
+                Animation = _animations.Find(s => s.name.Equals("run"));
+                break;
+            case AnimationStates.BattleIdle:
+                Animation = _animations.Find(s => s.name.Equals("waitingforbattle"));
+                break;
+            default:
+                Animation = _animations.Find(s => s.name.Equals("idle"));
+                break;
+        }
+
+        return Animation;
+    }
+}
